f13b31d93682a06dd254e925e0600ab7032c74dd
leozizz <leonardocps2015@gmail.com>
2023-04-02 22:37:01 -0300

    Componentizando Banner

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/header';
+import Banner from './components/banner/banner';
 
 function App() {
   return (
     <div className="App">
-      {/* Componente Header */}
       <Header />
-
-      <div>
-        <iframe src="https://embed.lottiefiles.com/animation/140772"></iframe>
-        <p>Brading / UI / UX / Tecnologia</p>
-        <h1>Agência de Brandind</h1>
-        <h2>e design digital</h2>
-      </div>
-
+      <Banner />
       <div>
         <h3>Experiências de Trabalho</h3>
         <p>Há mais de 10 anos no mercado de Branding, Deisgn Gráfico, Criação de Sites e Marketing digital, nos empenhamos diariamente para entregar resultados que tragam impacto aos nossos clientes.</p>
---
+++ src/components/banner/banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './banner.css';
+
+const Banner = () => {
+    return (
+        <div className="banner-container">
+            <iframe src="https://embed.lottiefiles.com/animation/140772"></iframe>
+            <div>
+                <p>Brading / UI / UX / Tecnologia</p>
+                <h1>Agência de Brandind</h1>
+                <h2>e design digital</h2>
+            </div>
+        </div>
+    )
+};
+
+export default Banner;
