34976b6a5e03e5a48637678b792e61bae40896b1
leozizz <leonardocps2015@gmail.com>
2023-04-02 22:30:58 -0300

    Estrutura inicial da pagina

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* Componente Header */}
+      <Header />
+
+      <div>
+        <iframe src="https://embed.lottiefiles.com/animation/140772"></iframe>
+        <p>Brading / UI / UX / Tecnologia</p>
+        <h1>Agência de Brandind</h1>
+        <h2>e design digital</h2>
+      </div>
+
+      <div>
+        <h3>Experiências de Trabalho</h3>
+        <p>Há mais de 10 anos no mercado de Branding, Deisgn Gráfico, Criação de Sites e Marketing digital, nos empenhamos diariamente para entregar resultados que tragam impacto aos nossos clientes.</p>
+
+        <div>
+          <p>Data</p>
+          <h4>Titulo</h4>
+          <p>Empresa</p>
+          <p>Descrição</p>
+        </div>
+        
+        <div>
+          <p>Data</p>
+          <h4>Titulo</h4>
+          <p>Empresa</p>
+          <p>Descrição</p>
+        </div>
+        
+        <div>
+          <p>Data</p>
+          <h4>Titulo</h4>
+          <p>Empresa</p>
+          <p>Descrição</p>
+        </div>
+      </div>
+
+      <footer>
+        <p>M</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online utilizando estratégias, ferramentas e tecnologias personalizadas.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Icone</p>
+        <p>Icone</p>
+        <p>Icone</p>
+        <p>Icone</p>
+        <p>Icone</p>
+        <p>Icone</p>
+        <p>Copyright 2022</p>
+      </footer>
     </div>
   );
 }
