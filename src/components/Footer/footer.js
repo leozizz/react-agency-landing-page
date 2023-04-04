ef2ad33680c75a8898ac2455b3073e4997fc52fe
leozizz <leonardocps2015@gmail.com>
2023-04-03 22:33:07 -0300

    Aplicando estilos ao Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,19 +1,26 @@
 import React from "react";
+import './footer.css'
 
 const Footer = () => {
     return (
-        <footer>
-            <p>M</p>
-            <p>
-            Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online utilizando estratégias, ferramentas e tecnologias personalizadas.
-            </p>
-            <p>Icone</p>
-            <p>Icone</p>
-            <p>Icone</p>
-            <p>Icone</p>
-            <p>Icone</p>
-            <p>Icone</p>
-            <p>Copyright 2022</p>
+        <footer className="footer">
+            <div className="footer-container">
+                <img className="logo" src='./assets/logo.png' alt="logo"/>
+                <p>
+                Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online utilizando estratégias, ferramentas e tecnologias personalizadas.
+                </p>
+                <div className="social">
+                    <img src='./assets/facebook.png' alt="logo"/>
+                    <img src='./assets/twitter.png' alt="logo"/>
+                    <img src='./assets/linkedin.png' alt="logo"/>
+                    <img src='./assets/dribble.png' alt="logo"/>
+                    <img src='./assets/behance.png' alt="logo"/>
+                    <img src='./assets/google-plus.png' alt="logo"/>
+                </div>
+                <p className="copy">
+                    Copyright 2023 &copy; <a href="https://github.com/leozizz" target="_blank">leozizz</a>
+                </p>
+            </div>
         </footer>
     )
 };
